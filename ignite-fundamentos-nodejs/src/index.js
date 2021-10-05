b773b17654eeaf126e1250fc1fba36019fa127b8
CarolineCunha <caroline.cunha.b@hotmail.com>
2021-10-05 19:16:13 -0300

    tipos de parâmetros

--- ignite-fundamentos-nodejs/src/index.js
+++ ignite-fundamentos-nodejs/src/index.js
@@ -2,15 +2,19 @@ const { response } = require('express');
 const express = require('express');
 
 const app = express();
+app.use(express.json());
 
 app.get("/courses", (request,response) => {
+    const query = request.query;
     return response.json(["Curso 1", "Curso 2", "Curso 3"]);
 });
 app.post("/courses", (request,response) =>{
+    const body = request.body;
     return response.json(["Curso 1", "Curso 2", "Curso 3","Curso 4"]);
 });
 app.put("/courses/:id", (request,response) => {
     return response.json(["Curso 6", "Curso 2", "Curso 3","Curso 4"]);
+    const {id} = request.params;
 });
 app.patch("/courses/:id", (request,response) => {
     return response.json(["Curso 6", "Curso 7", "Curso 3","Curso 4"]);
