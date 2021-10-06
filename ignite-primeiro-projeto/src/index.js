820039fe3b0026a7c161a8181659ece3933e746e
CarolineCunha <caroline.cunha.b@hotmail.com>
2021-10-06 13:07:53 -0300

    listando extrato

--- ignite-primeiro-projeto/src/index.js
+++ ignite-primeiro-projeto/src/index.js
@@ -1,3 +1,4 @@
+const { response } = require("express");
 const express = require("express");
 const { v4: uuidv4 } = require("uuid")
 const app = express();
@@ -21,4 +22,10 @@ app.post("/account", (request,response) => {
     return response.status(201).send();
 });
 
+app.get("/statement/:cpf", (request,reponse) => {
+    const { cpf } = request.params;
+
+    const customers = customers.find(customer => customer.cpf === cpf);
+    return response.json(customer.statement);
+})
 app.listen(3333);
