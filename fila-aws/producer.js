93eb55b25c2e2b8d55a03a5f529ed528b0641c28
CarolineCunha <caroline.cunha.b@hotmail.com>
2021-10-07 10:34:15 -0300

    recebendo msgs

--- fila-aws/producer.js
+++ fila-aws/producer.js
@@ -1,11 +1,39 @@
-(async () => {
-    require('dotenv').config();
-    const sqsClient = require('./sqsClient');
- 
-    try {
-        const result = await sqsClient.sendMessage({ id: 1, action: 'x' });
-        console.log(result);
-    } catch (error) {
-        console.log(`ERRO: ${error}`);
+// Load the AWS SDK for Node.js
+var AWS = require('aws-sdk');
+// Set the region 
+AWS.config.update({region: 'us-east-1'});
+
+// Create an SQS service object
+var sqs = new AWS.SQS({apiVersion: '2012-11-05'});
+
+var params = {
+   // Remove DelaySeconds parameter and value for FIFO queues
+  DelaySeconds: 10,
+  MessageAttributes: {
+    "Title": {
+      DataType: "String",
+      StringValue: "The Whistler"
+    },
+    "Author": {
+      DataType: "String",
+      StringValue: "John Grisham"
+    },
+    "WeeksOn": {
+      DataType: "Number",
+      StringValue: "6"
     }
-})();
+  },
+  MessageBody: "Information about current NY Times fiction bestseller for week of 12/11/2016.",
+  // MessageDeduplicationId: "TheWhistler",  // Required for FIFO queues
+  // MessageGroupId: "Group1",  // Required for FIFO queues
+  QueueUrl: "https://sqs.us-east-1.amazonaws.com/328367910941/teste.fifo"
+  
+};
+
+sqs.sendMessage(params, function(err, data) {
+  if (err) {
+    console.log("Error", err);
+  } else {
+    console.log("Success", data.MessageId);
+  }
+});
