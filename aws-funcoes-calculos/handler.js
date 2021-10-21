21044a59f91199ef5da54c18df9812a7400a41b3
CarolineCunha <caroline.cunha.b@hotmail.com>
2021-10-21 08:14:38 -0300

    subtracao

--- aws-funcoes-calculos/handler.js
+++ aws-funcoes-calculos/handler.js
@@ -11,6 +11,20 @@ module.exports.soma = async (event) => {
     statusCode: 200,
     body: JSON.stringify({message:`${soma}`})
   }
-    return response;
+  return response;
   };
 
+
+module.exports.subtracao = async (event) => {
+  x = Number(event.key1)
+  y = Number(event.key2)
+
+  var subtracao  = x - y
+  console.log(soma)
+
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({message:`${subtracao}`})
+  }
+  return response;
+}
