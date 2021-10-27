4c92b83b6355ca44576700ecc951ceab2f40a391
CarolineCunha <caroline.cunha.b@hotmail.com>
2021-10-27 07:07:00 -0300

    mult

--- aws-funcoes-calculos/handler.js
+++ aws-funcoes-calculos/handler.js
@@ -14,7 +14,6 @@ module.exports.soma = async (event) => {
   return response;
   };
 
-
 module.exports.subtracao = async (event) => {
   x = Number(event.key1)
   y = Number(event.key2)
@@ -25,6 +24,20 @@ module.exports.subtracao = async (event) => {
   const response = {
     statusCode: 200,
     body: JSON.stringify({message:`${subtracao}`})
+  }
+  return response;
+}
+
+module.exports.multiplicacao = async (event) => {
+  x = Number(event.key1)
+  y = Number(event.key2)
+
+  var multiplicacao  = x - y
+  console.log(soma)
+
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({message:`${multiplicacao}`})
   }
   return response;
 }
