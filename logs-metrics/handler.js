2e6ae9d2d37d2205a25e97d6cb2476e9f8b8678f
CarolineCunha <caroline.cunha.b@hotmail.com>
2021-10-29 17:51:47 -0300

    testes

--- logs-metrics/handler.js
+++ logs-metrics/handler.js
@@ -3,7 +3,7 @@ const AWS = require('aws-sdk');
 const cloudwatchlogs = new AWS.CloudWatchLogs();
 const cloudwatch = new AWS.CloudWatch();
 const logGroupNames = '/aws/lambda/logs-metrics-dev-log'.split(',');
-const queryString = 'fields @timestamp,  @message| filter @message like /INFO/| parse @message "INFO" as info| stats count(info) as result';
+const queryString = 'fields @timestamp,  @message| filter @message like /INFO/| parse @message "INFO" as info| stats count(info)/2 as result';
 const queryPeriod = 3000;
 const queryDelay = 1;
 const queryRetry = 10;
@@ -15,10 +15,11 @@ function sleep(ms) {
 
 module.exports.log = async(event, context) => {
   const endTime = Math.floor(new Date().getTime() / 1000);
-  const startT = endTime - (5 * 60);
+  const startT = endTime - (6 * 60);
   console.log("Asd")
   console.log(endTime)
   console.log(startT)
+
   const paramsStartQuery = {
     endTime: endTime,
     logGroupNames,
@@ -27,8 +28,8 @@ module.exports.log = async(event, context) => {
     limit: 1,
   };
   return cloudwatchlogs.startQuery(paramsStartQuery).promise().then(async(data) => {
-    let queryResults;
-    let status = '';
+    var queryResults;
+    var status = '';
     while (!['Complete', 'Failed', 'Cancelled'].includes(status)) {
       queryResults = await cloudwatchlogs.getQueryResults({ queryId: data.queryId }).promise().then(sleep(queryRetry)).catch((err) => { console.log(err); return { status: 'Failed' }; });
       ({ status } = queryResults);
@@ -36,20 +37,25 @@ module.exports.log = async(event, context) => {
     }
     if (status === 'Complete') {
       const metrics = [{
-        MetricName: 'DSEvent',
+        MetricName: '% of Success',
         Value: queryResults.statistics.recordsMatched,
         Timestamp: endTime
       }]
       const paramsPutMetricData = {
         MetricData: metrics,
-        Namespace: 'DSEvent',
+        Namespace: 'teste-carol',
       };
       await cloudwatch.putMetricData(paramsPutMetricData).promise().catch((err) => { console.log(err); });
-      return queryResults.statistics.recordsMatched;
+      console.log('oi')
+      console.log(queryResults.statistics)
+      console.log(JSON.stringify(queryResults))
+      console.log(queryResults.results[0][0].value)
+      return queryResults.results[0][0].value;
     }
     else {
       console.log('Query failed');
       return `Query ${data.queryId} failed:\n${queryString}`;
     }
   }).catch((err) => { console.log(err); });
+
 };
