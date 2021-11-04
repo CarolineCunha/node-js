d4453838be09ea99646fd378192ad61a2e0f3334
CarolineCunha <caroline.cunha.b@hotmail.com>
2021-11-04 14:33:06 -0300

    agora vai

--- logs-metrics/handler.js
+++ logs-metrics/handler.js
@@ -2,9 +2,14 @@ const AWS = require('aws-sdk');
 
 const cloudwatchlogs = new AWS.CloudWatchLogs()
 const cloudwatch = new AWS.CloudWatch()
-const logGroupNames = '/aws/lambda/logs-metrics-dev-log'.split(',')
-const queryString = 'fields @timestamp,  @message| filter @message like /INFO/| parse @message "INFO" as info| stats count(info)/2 as result'
-const queryRetry = 10;
+const logGroupName = process.env.LOG_GROUP_NAME
+const queryString = `fields @timestamp,  @message
+                      | filter @message like /INFO/
+                      | parse @message "INFO" as info
+                      | stats count(info)/2 as result`
+console.log(logGroupName)
+//logGroupName.split(',')
+console.log(logGroupName)
 
 module.exports.log = async(event, context) => {
   const endTime = Math.floor(new Date().getTime() / 1000)
@@ -14,48 +19,51 @@ module.exports.log = async(event, context) => {
 
   const paramsStartQuery = {
     endTime: endTime,
-    logGroupNames,
+    logGroupName,
     queryString,
     startTime: startT,
     limit: 1,
-  };
-  return cloudwatchlogs.startQuery(paramsStartQuery).promise().then(async(data) => {
-    var queryResults
-    var status = ''
-    
-    while (!['Complete', 'Failed', 'Cancelled'].includes(status)) {
-      queryResults = await cloudwatchlogs.getQueryResults({ queryId: data.queryId }).promise().catch((err) => { 
-        console.log(err) 
-        return { status: 'Failed' } 
-        });
-        ({ status } = queryResults)
-      console.log(status)
-    }
+  }
 
-     if (status === 'Complete') {
-      const metrics = [{
-        MetricName: '% of Success1',
-        Value: queryResults.results[0][0].value
-      }] 
-
-      const paramsPutMetricData = {
-        MetricData: metrics,
-        Namespace: 'teste-caroline'
+  try {
+    const data = await cloudwatchlogs.startQuery(paramsStartQuery).promise()
+      var queryResults
+      var status = ''
+      
+      while (!['Complete', 'Failed', 'Cancelled'].includes(status)) {
+        queryResults = await cloudwatchlogs.getQueryResults({ queryId: data.queryId }).promise().catch((err) => { 
+          console.log(err) 
+          return { status: 'Failed' } 
+          });
+          ({ status } = queryResults)
+        console.log(status)
       }
-      await cloudwatch.putMetricData(paramsPutMetricData).promise().catch((err) => { 
-        console.log(err)
-      })
-      console.log('oi')
-      console.log(queryResults.statistics)
-      console.log(JSON.stringify(queryResults))
-      console.log(`Query ${queryResults.results[0][0].value}`)
-      return queryResults.results[0][0].value
-    }
-    else {
-      console.log('Query failed')
-      return `Query ${data.queryId} failed:\n${queryString}`
-    }
-  }).catch((err) => { console.log(err) 
-  })
-
+  
+       if (status === 'Complete') {
+        const metrics = [{
+          MetricName: '% of Success1',
+          Value: queryResults.results[0][0].value
+        }] 
+  
+        const paramsPutMetricData = {
+          MetricData: metrics,
+          Namespace: 'teste-caroline'
+        }
+        await cloudwatch.putMetricData(paramsPutMetricData).promise().catch((err) => { 
+          console.log(err)
+        })
+        console.log('oi')
+        console.log(queryResults.statistics)
+        console.log(JSON.stringify(queryResults))
+        console.log(`Query ${queryResults.results[0][0].value}`)
+        return queryResults.results[0][0].value
+      }
+      else {
+        console.log('Query failed')
+        return `Query ${data.queryId} failed:\n${queryString}`
+      }
+  }
+  catch(err) {
+    console.log(err)
+  }
 }
