fdbbf72141fbd0d7add5c31e5236526456f96a61
CarolineCunha <caroline.cunha.b@hotmail.com>
2021-11-25 07:01:45 -0300

    create table

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,3 +12,21 @@ resource "aws_s3_bucket" "my-test-bucket" {
     Managedby = "Terraform"
   }
 }
+
+resource "aws_dynamodb_table" "basic-dynamodb-table" {
+  name           = "GameScores"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 20
+  write_capacity = 20
+  hash_key       = "UserId"
+
+  attribute {
+    name = "UserId"
+    type = "S"
+  }
+
+  tags = {
+    Name        = "dynamodb-table-1"
+    Environment = "dev"
+  }
+}
