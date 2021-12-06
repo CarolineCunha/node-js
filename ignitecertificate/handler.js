72219ae241a849356f06e3d2b2f5a4364c07bfa0
CarolineCunha <caroline.cunha.b@hotmail.com>
2021-12-06 06:50:03 -0300

    removendo

--- ignitecertificate/handler.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports.teste = async function(event, context) {
-  event.Records.forEach(record => {
-    const { body } = record;
-    console.log(body);
-  });
-  return {};
-}
